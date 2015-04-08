966e5660fca8e1db456dd37832e77ae5a9646510
nullpos <Greenstar77@gmail.com>
2015-04-08 23:17:27 +0900

    add Const class

---
+++ src/util/Const.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Const {
+    private Const() {};
+    
+    public static final int SPACE = 0;
+    public static final int WHITE = 1;
+    public static final int BLACK = 2;
+    
+}
