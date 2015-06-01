155abceb3b24ad26f1c9419cfa4e387a6393cb68
nullpos <Greenstar77@gmail.com>
2015-06-01 21:48:06 +0900

    Geneの出力方法を追加、学習結果を更新

--- src/learning/AbstructGene.java
+++ src/learning/AbstructGene.java
@@ -5,55 +5,55 @@ import util.*;
 public abstract class AbstructGene implements Sortable {
     protected byte[] chromosome; // byte -> -127~128
     protected double fitness;
-    
+
     public AbstructGene(int n) {
         chromosome = new byte[n]; //
         fitness = 0.0;
     }
-    
+
     public AbstructGene(Gene g) {
         chromosome = new byte[g.chromosome.length];
         System.arraycopy(g.chromosome,0,chromosome,0,chromosome.length);
         fitness=g.fitness;
     }
-    
+
     public abstract Object clone();
-    
+
     public abstract void calcFitness(Gene g);
-    
+
     public double getFitness(){ return fitness; }
-    
+
     public void setChromosome(byte[] c) {
         for (int i = 0; i < chromosome.length; i++) {
             chromosome[i] = c[i];
         }
     }
-    
+
     public boolean isBefore(Sortable s){
         return fitness > ((Gene)s).fitness;
     }
-    
+
     public boolean isAfter(Sortable s){
         return fitness < ((Gene)s).fitness;
     }
-    
+
     public byte getRandomChromosome(){
         return (byte) (Math.random() * (Byte.MAX_VALUE - Byte.MIN_VALUE + 1));
     }
-    
+
     public void initRandom(){
         for(int i = 0; i < chromosome.length; i++) {
             chromosome[i] = getRandomChromosome();
         }
     }
-    
+
     /**
         一様交叉 遺伝子全体をランダムに交叉
-    */
+     */
     public static void flatCrossover(Gene g1,Gene g2,double p){
         int len = g1.chromosome.length;
         if (len > g2.chromosome.length) len = g2.chromosome.length;
-        
+
         for(int i = 0; i < len; i++) {
             if(Math.random() < p) {
                 byte d = g1.chromosome[i];
@@ -62,15 +62,15 @@ public abstract class AbstructGene implements Sortable {
             }
         }
     }
-    
+
     /**
         1点交叉 ランダムな1点のみ交叉
-    */
+     */
     public static void onePointCrossover(Gene g1,Gene g2){
         int len=g1.chromosome.length;
         if(len>g2.chromosome.length) len=g2.chromosome.length;
         int r=(int)(Math.random()*len);
-        
+
         for(int i=r;i<len;i++){
             byte d=g1.chromosome[i];
             g1.chromosome[i]=g2.chromosome[i];
@@ -81,20 +81,30 @@ public abstract class AbstructGene implements Sortable {
     /**
         突然変異
         与えられた個数の染色体をランダムに変更します。
-    */
+     */
     public void mutate(int n){
         for(int i=0;i<n;i++){
             int r=(int)(Math.random()*chromosome.length);
             chromosome[r]=getRandomChromosome();
         }
     }
-
+    
     public String toString(){
         String str="";
         for(int i=0;i<chromosome.length;i++){
             String s="00"+Integer.toString((int)(chromosome[i]+Byte.MAX_VALUE),16);
             str=str+(s.substring(s.length()-2));
         }
-    return str;
-  }
+        return str;
+    }
+    
+    public String toString2() {
+        String str;
+        str = "{"+this.chromosome[0];
+        for (int j = 1; j < this.chromosome.length; j++) {
+            str += ", " + this.chromosome[j];
+        }
+        str += "}";
+        return str;
+    }
 }
--- src/learning/Main.java
+++ src/learning/Main.java
@@ -11,23 +11,23 @@ import util.QSort;
 
 public class Main {
     public static void main(String[] args) {
-        int max_generation = 30; // 最大世代数
+        int max_generation = 80; // 最大世代数
         int gene_num = Const.GENE_NUM; // 遺伝子集団の数
         int chromo_num = Const.CHROMO_NUM; // chromosomeの数
         int crossover_type = 1; // 交叉の方法
         int sel_num = (int) (gene_num * 0.1); // ランダムでsel_num個の遺伝子を選び、複製した遺伝子列を作成する
-        int[] mutate = {(int) (gene_num * 0.5), 6}; // 突然変異の方法
-        byte[][] teachers = Const.GENE_TEACHER[4];
+        int[] mutate = {(int) (gene_num * 0.7), 6}; // 突然変異の方法
+        byte[][] teachers = Const.GENE_TEACHER[7];
         
         Calendar now = Calendar.getInstance();
-        String file_name = Const.GENE_DIR + 
+        String genes_file_name = Const.GENE_DIR + 
                 ((now.get(Calendar.HOUR_OF_DAY) < 10) ? "0"+now.get(Calendar.HOUR_OF_DAY) : now.get(Calendar.HOUR_OF_DAY)) +"-"+ 
                 ((now.get(Calendar.MINUTE) < 10) ? "0"+now.get(Calendar.MINUTE) : now.get(Calendar.MINUTE)) +"-"+
                 ((now.get(Calendar.SECOND) < 10) ? "0"+now.get(Calendar.SECOND) : now.get(Calendar.SECOND)) + ".txt";
-        
+
         PrintStream ps = null;
         try {
-            FileOutputStream fos = new FileOutputStream(file_name, true);
+            FileOutputStream fos = new FileOutputStream(genes_file_name, true);
             ps = new PrintStream(fos);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -51,7 +51,7 @@ public class Main {
         }
         //*/
         
-        (new File(file_name)).delete();
+        (new File(genes_file_name)).delete();
         GA ga = new GA(g);
         double tmp = Double.NEGATIVE_INFINITY;
         long start = System.currentTimeMillis();
@@ -96,11 +96,7 @@ public class Main {
         Gene elite = ga.getElite();
         ps.println(elite.getFitness());
         ps.println(elite.toString());
-        ps.print("{"+elite.chromosome[0]);
-        for (int j = 1; j < elite.chromosome.length; j++) {
-            ps.print(", " + elite.chromosome[j]);
-        }
-        ps.println("}");
+        ps.println(elite.getFitness()+elite.toString2());
         
         long end = System.currentTimeMillis();
         System.out.println("time: " + ((end - start) / 1000) + "s");
