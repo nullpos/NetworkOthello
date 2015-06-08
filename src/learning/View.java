337073e921a9427e675007158d23ffb31aebaa8f
nullpos <Greenstar77@gmail.com>
2015-06-08 23:07:17 +0900

    デモ用

--- src/learning/View.java
+++ src/learning/View.java
@@ -11,12 +11,11 @@ import util.Images;
 
 public class View {
     private static final byte[][] genes = {
-        {-103, -104, -99, -81, -94, -58, 21, -49, 85, 42, -104, 79, 125, -8, -14, 74, -120, -66, -8, 25, 74, 24, -73, 47, 99, 104, -28, -5, -20, -120, -113, -5, -75, 2, 74, 126},
-        {22, -35, 56, -99, -118, -13, 50, -74, 25, 75, -76, 32, -99, -75, -16, 98, -109, -19, 38, 80, 127, 119, -118, 27, 89, 105, 11, -15, -64, -109, -107, -56, -106, -70, 122, 108}
+        Const.GENE_ANS[4],Const.GENE_ANS[4]
     };
     
     public static void main(String[] args) {
-        int comLevel = 0;
+        int comLevel = 2;
         Images images = new Images();
         try {
             ResourceBundle rb = ResourceBundle.getBundle(Const.SETTINGS_NAME);
